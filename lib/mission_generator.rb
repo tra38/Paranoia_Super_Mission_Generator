a03159d85bb6e983e7bc4207424cc916c9ccd2aa
tra38 <tra38@nau.edu>
2018-11-15 08:30:31 -0600

    Add Batclone mission

--- lib/mission_generator.rb
+++ lib/mission_generator.rb
@@ -577,7 +577,8 @@ class MissionGenerator
         "We need you to infiltrate a suspected Commie base at {@building_name}. You must locate the base,get inside and then plant this homing beacon so our Warbot can eliminate it.",
         "We’ve had to terminate the committee responsible for running {@building_name}. Even though you have no applicable skills or training and don’t have the Clearance to know what the committee does, we need you to take over.",
         "{@overdog_name} needs your help with their latest dinner party/round of UltraGolf/nefarious secret project.",
-        "R&D needs to do certain...things...at {@building_name}. Escort the scholars. Don't pay attention to the Lovecraftian rantings. Let them do their occult ritual, then escort them back. (We suspect that {random_target} may want to interfere with the ritual.)"
+        "R&D needs to do certain...things...at {@building_name}. Escort the scholars. Don't pay attention to the Lovecraftian rantings. Let them do their occult ritual, then escort them back. (We suspect that {random_target} may want to interfere with the ritual.)",
+        "The {@sector_name} Batclone is a highly dangerous vigilante who has repeatedly attacked and harmed high-clearance citizens. See if you can recruit them into Internal Security. If they refuse, then kill them. (GM Note: Batclone is secretly {@overdog_name}, who has made many high-clearance enemies during their rise to power.)"
 
       complication "A service group feud between {two_rival_groups} have spun out of control in {@sector_name}. Assassinations and drive-by shootings are commonplace, and the sector is close to civil war.",
                   "A major malfunction has rendered {@sector_name} uninhabitable; the local environment kills an unprotected citizen in minutes if not seconds.",
